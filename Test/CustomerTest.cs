78c830d0bfcc28eafec6005cb84ace885edb197b
Andrew Chaa <andrew.chaa@yahoo.co.uk>
2011-10-09 10:34:09 +0100

    Should Widhdraw All The Money

--- Test/CustomerTest.cs
+++ Test/CustomerTest.cs
@@ -7,33 +7,42 @@ namespace Test
     [TestFixture]
     public class CustomerTest
     {
+        private Customer _customer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var bank = new Bank(new EmptyNicknameValidator(), new DuplicatedNicknameValidator());
+            _customer = bank.AddCustomer("Andy", new DateTime(1981, 01, 01));
+        }
+
         [Test]
         public void ShouldDepositMoneyAndSeeItReflectInTheBalance()
         {
-            Customer customer = GetCustomer();
-
-            customer.Deposit(100);
-            decimal balance = customer.GetBanalce();
+            _customer.Deposit(100);
+            decimal balance = _customer.GetBanalce();
 
             Assert.That(balance, Is.EqualTo(100));
-        }
-
-        private Customer GetCustomer()
-        {
-            var bank = new Bank(new EmptyNicknameValidator(), new DuplicatedNicknameValidator());
-            return bank.AddCustomer("Andy", new DateTime(1981, 01, 01));
         }
 
         [Test]
         public void ShouldWithdrawFromTheBalance()
         {
-            var customer = GetCustomer();
-
-            customer.Deposit(200);
-            customer.Withdraw(50);
-            decimal balance = customer.GetBanalce();
+            _customer.Deposit(200);
+            _customer.Withdraw(50);
+            decimal balance = _customer.GetBanalce();
 
             Assert.That(balance, Is.EqualTo(150));
+        }
+
+        [Test]
+        public void ShouldWithdrawAllTheMoney()
+        {
+            _customer.Deposit(500);
+            _customer.Withdraw(500);
+            decimal balance = _customer.GetBanalce();
+
+            Assert.That(balance, Is.EqualTo(0));
         }
     }
 }
